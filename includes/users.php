7092ca32aa8cc2f9db8ac262aff31d6134c53cd7
David Shakir <dshakir@archermalmo.com>
2016-01-14 12:43:36 -0600

    Init template for users

---
+++ includes/users.php
@@ -0,0 +1,99 @@
+<?php
+
+include_once('db_config.php');
+
+$users_stmt = $conn->prepare("SELECT * FROM users");
+$users_stmt->execute();
+$data = $users_stmt->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Login</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="scripts/script.js"></script>
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="container">
+<!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Logo</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                    <li>
+                        <a href="register.php">Register</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+	<div class="row">
+		<div class="table-responsive" style="clear:both;">
+			<p></p>
+			<p></p>
+			<table class="table table-striped">
+				<tr>
+					<th>ID</th>
+					<th>Username</th>
+					<th>Email</th>
+					<th>Signup Date</th>
+				</tr>
+				<?php
+					foreach ($data as $row) {
+						echo '<tr>';
+							echo "<td>{$row['user_id']}</td>";
+							echo "<td>{$row['username']}</td>";
+							echo "<td>{$row['email']}</td>";
+							echo "<td>{$row['signup_date']}</td>";
+						echo '</tr>';
+					}
+
+				 ?>
+			</table>
+		</div>
+	</div>
+</div>
+
+    <!-- JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script type="text/javascript" src-"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+
+
+</body>
+</html>
